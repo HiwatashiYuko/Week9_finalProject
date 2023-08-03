05bc2d59005c6221d8fcae443346d59314f5438a
Akiko571 <akikoume@gmail.com>
2023-08-03 11:21:11 +0900

    コメント入力フォームをReact.FCを使用しない形に修正

--- 230729chatgpt-app/client/components/CommentForm.tsx
+++ 230729chatgpt-app/client/components/CommentForm.tsx
@@ -6,20 +6,24 @@ interface CommentFormProps {
   onSubmit: (comment: string) => void;
 }
 
-const CommentForm: React.FC<CommentFormProps> = ({ onSubmit }) => {
+function CommentForm({ onSubmit }:CommentFormProps) {
   const [comment, setComment] = useState('');
 
-  const handleSubmit = (e: FormEvent) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     onSubmit(comment);
     setComment('');
+  };
+
+  const handleCommentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <textarea
         value={comment}
-        onChange={(e) => setComment(e.target.value)}
+        onChange={handleCommentChange}
         placeholder="コメントを入力してください"
         rows={4}
         cols={100}
