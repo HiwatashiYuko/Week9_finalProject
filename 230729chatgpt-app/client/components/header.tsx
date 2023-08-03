2e8c2ea2b6dc24e83950779fe14be1a86bdd3522
Akiko571 <akikoume@gmail.com>
2023-08-03 23:32:52 +0900

    headerコンポーネント作成(各ページへのリンクのみ)

---
+++ 230729chatgpt-app/client/components/header.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <Link href="/home">ほめてもらう</Link>
+      </div>
+      {/* リンク要修正 */}
+      <div>
+        <Link href="/3good">今日の良かったことを記録する</Link>
+      </div>
+      <div>
+        <Link href="/chat">今までの良かったことを見る</Link>
+      </div>
+      <div>
+        <Link href="/progress">「小さな目標」の進みを確認</Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
