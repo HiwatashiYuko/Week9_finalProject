e51ceada0f01fd93a50fb6232e6b847aac966863
Akiko571 <akikoume@gmail.com>
2023-08-04 14:15:37 +0900

    tailwind.config.jsに色の指定を記載

--- 230729chatgpt-app/client/tailwind.config.js
+++ 230729chatgpt-app/client/tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          210: '#D8EFE3',
+          410: '#8FDBB5',
+          420: '#B2E7CE',
+          430: '#aad4c7',
+          510: '#6dc0a9',
+        },
+        yellow: {
+          510: '#f4d6b8',
+          520: '#f7a65e',
+        },
+        grey: {
+          110: '#e6e6e6',
+          120: '#FFF8F9',
+        }
+      }
+    },
   },
   plugins: [],
 }
