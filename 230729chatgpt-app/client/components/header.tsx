8452dd3080194d7fed8a1b9b914773312e39732d
Akiko571 <akikoume@gmail.com>
2023-08-04 11:46:23 +0900

    ヘッダーを横並びに修正

--- 230729chatgpt-app/client/components/header.tsx
+++ 230729chatgpt-app/client/components/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header>
+    <header className="flex justify-between">
       <div>
         <Link href="/home">ほめてもらう</Link>
       </div>
