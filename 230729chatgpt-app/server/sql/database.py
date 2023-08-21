fabd007c471589518d76bdaa615b2f8c0a4b3d87
HiwatashiYuko <sarappi3.72kero4@gmail.com>
2023-08-21 11:03:30 +0900

    CORSをクライアントディレクトリにインストール

--- 230729chatgpt-app/server/sql/database.py
+++ 230729chatgpt-app/server/sql/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+# from sql.setting import DATABASE_URL
 from sql.setting import DATABASE_URL
 from sqlalchemy.ext.declarative import declarative_base
 
